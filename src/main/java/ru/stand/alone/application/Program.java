72623aeb047f62ebacf409e6f09a53e78df4b060
Сергей <sergzv22@rambler.ru>
2022-07-01 20:53:46 +0400

    the_program_is_finished

--- src/main/java/ru/stand/alone/application/Program.java
+++ src/main/java/ru/stand/alone/application/Program.java
@@ -1,20 +1,53 @@
 package main.java.ru.stand.alone.application;
 
-import java.util.Scanner;
+import java.util.Date;
 
 public class Program
 {
     public void main()
     {
         System.out.println("Введите строку которую следует инвертировать:");
-
-        Scanner scanner = new Scanner(System.in);
+        System.out.println();
 
         String userInput = System.console().readLine();
-        System.out.println("Ваша строка: " + invertString(userInput));
+        System.out.println();
 
+        System.out.println("Ваша строка: " + userInput);
+        System.out.println("Инвертированная строка: " + invertString(userInput));
+        System.out.println();
 
+        outputResultForInversions(1000, userInput);
+        outputResultForInversions(10000, userInput);
+        outputResultForInversions(100000, userInput);
 
+        System.out.println("Чтобы выйти нажмите на крестик в верхнем правом углу консоли.");
+
+        while (true) System.console().readLine();
+    }
+
+    private void outputResultForInversions(int repeatCounts, String userInput)
+    {
+        long timeSpan = measureTimeInversionsForTimes(repeatCounts, userInput);
+        System.out.println(repeatCounts + " инвертирований произвелось за: " + timeSpan + " мс.");
+        System.out.println();
+    }
+
+    private long measureTimeInversionsForTimes(int repeatCounts, String invertedString)
+    {
+        long measureStart;
+        long measureEnd;
+
+        measureStart = new Date().getTime();
+        invertTimes(repeatCounts, invertedString);
+        measureEnd = new Date().getTime();
+
+        return measureEnd - measureStart;
+    }
+
+    private void invertTimes(int repeatCounts, String invertedString)
+    {
+        for (int i = 0; i < repeatCounts; i++)
+            invertString(invertedString);
     }
 
     private String invertString(String stringToInvert)
